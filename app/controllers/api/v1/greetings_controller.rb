81cf7d245626d4d616359e0f49cb9e0b4e4c9336
keebot-png <kee.bot@icloud.com>
2023-04-11 13:23:37 +0200

    Finish final changes to api.

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::GreetingsController < ApplicationController
+    def index
+      greeting = Greeting.pluck(:salutations).sample
+      render json: greeting, status: :ok
+    end
+  end
+  
